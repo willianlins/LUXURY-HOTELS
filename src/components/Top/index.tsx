6d14536d1e9d7dbf08020e08308c1fb5d2530d7f
Willian Lins Araújo <willian.araujo@anchieta.br>
2021-12-22 18:30:27 -0300

    bl

--- src/components/Top/index.tsx
+++ src/components/Top/index.tsx
@@ -6,28 +6,32 @@ import styles from "./styles.module.scss";
 export function Top() {
   return (
     <header className={styles.headerContainer}>
-      <div>
-        <div>
-          <h1>LUXURY <pre>HOTELS</pre></h1>
+      <div className={styles.headerContent}>
+        <div className={styles.headerTop}>
+          <div>
+            <img src="./assets/logo.svg" alt="Luxuria Hotels" />
+          </div>
+          <nav>
+            <ul>
+              <li><a href="https://">Home</a></li>
+              <li><a href="https://">Facilities</a></li>
+              <li><a href="https://">Rooms</a></li>
+              <li><a href="https://">Contact-us</a></li>
+            </ul>
+          </nav>
         </div>
-        <nav>
-          <ul>
-            <li><a href="https://">Home</a></li>
-            <li><a href="https://">Facilities</a></li>
-            <li><a href="https://">Rooms</a></li>
-            <li><a href="https://">Contact-us</a></li>
-          </ul>
-        </nav>
-      </div>
-      <h2>Welcome to <pre>Luxury</pre> <b>Hotels</b></h2>
-      <p>Book your stay and enjoy Luxury redefined at the most affordable rates.</p>
-      <button>
-        <BsFillCalendarCheckFill />
-        Book now
-      </button>
-      <div>
-        <span>Scroll</span>
-        <BsFillArrowDownCircleFill />
+        <div>
+          <h2>Welcome to <pre>Luxury</pre> <b>Hotels</b></h2>
+          <p>Book your stay and enjoy Luxury<br /> redefined at the most affordable rates.</p>
+        </div>
+        <button>
+          <BsFillCalendarCheckFill />
+          Book now
+        </button>
+        <div>
+          <span>Scroll</span>
+          <BsFillArrowDownCircleFill />
+        </div>
       </div>
     </header>
   );
